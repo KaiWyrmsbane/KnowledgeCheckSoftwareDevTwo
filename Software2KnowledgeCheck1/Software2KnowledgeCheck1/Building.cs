3d9b340935b10e0d568adc06a713dcef68229c67
james.vanmeter <james.vanmeter@CO-JAMES-WX>
2022-10-06 13:33:50 -0400

    Made CreateApartment a generic class

--- Software2KnowledgeCheck1/Software2KnowledgeCheck1/Building.cs
+++ Software2KnowledgeCheck1/Software2KnowledgeCheck1/Building.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Software2KnowledgeCheck1
 {
-    internal class Building
+    //CreateApartmentClass
+    internal class CreateApartment <T>: Building
+    {
+        public void ConstructBuildingType(Building building)
+        {
+                //Get materials
+                var materialRepo = new MaterialsRepo();
+                var materialsNeeded = materialRepo.GetMaterials();
+
+                var permitRepo = new ZoningAndPermitRepo();
+
+                var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+
+                if (buildingWasMade)
+                {
+                  Buildings.Add(building);
+                }
+        }
+    }
+
+    internal class Building: City 
     {
         //I want the construction to be inside the Building class
         public string Street { get; set; }
@@ -14,21 +34,21 @@ namespace Software2KnowledgeCheck1
         public int StreetNumber { get; set; }
 
         
-        public void CreateApartment(Apartment apartment)
-        {
-            // Get materials
-            var materialRepo = new MaterialsRepo();
-            var materialsNeeded = materialRepo.GetMaterials();
+        //public void CreateApartment(Apartment apartment)
+        //{
+        //    // Get materials
+        //    var materialRepo = new MaterialsRepo();
+        //    var materialsNeeded = materialRepo.GetMaterials();
 
-            var permitRepo = new ZoningAndPermitRepo();
+        //    var permitRepo = new ZoningAndPermitRepo();
 
-            var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
+        //    var buildingWasMade = ConstructBuilding<Apartment>(materialsNeeded, permitRepo.GetPermit(), permitRepo.ZoningApproves());
 
-            if (buildingWasMade)
-            {
-                Buildings.Add(apartment);
-            }
-        }
+        //    if (buildingWasMade)
+        //    {
+        //        Buildings.Add(apartment);
+        //    }
+        //}
 
         public bool ConstructBuilding<T>(List<string> materials, bool permit, bool zoning) where T: Building
         {
